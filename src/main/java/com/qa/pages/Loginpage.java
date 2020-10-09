d24943426aeea2e9be63f090558d3195afa771d6
unknown <venkatramreddyg@gmail.com>
2020-10-09 14:00:28 +0530

    Login and search page code change

--- src/main/java/com/qa/pages/Loginpage.java
+++ src/main/java/com/qa/pages/Loginpage.java
@@ -5,4 +5,8 @@ public class Loginpage {
 	public void Loginpage(){
 		System.out.println("Login page");
 	}
+	
+	public void loginset(){
+		System.out.println("login set");
+	}
 }
---
+++ src/main/java/com/qa/pages/Searchpage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class Searchpage {
+	
+	public void searchpage(){
+		System.out.println("Search page");
+	}
+
+}
